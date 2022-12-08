1074d17700a9779973c5c2ab0d94d6809594b687
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-08 19:53:39 +0200

    refactor controllers
    - delete constructors;
    - add @RequiredArgsConstructor.

--- src/main/java/com/bondarenko/movieland/web/controller/GenreController.java
+++ src/main/java/com/bondarenko/movieland/web/controller/GenreController.java
@@ -2,6 +2,7 @@ package com.bondarenko.movieland.web.controller;
 
 import com.bondarenko.movieland.entity.Genre;
 import com.bondarenko.movieland.service.GenreService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GenreController {
     private final GenreService genreService;
-
-    public GenreController(GenreService genreService) {
-        this.genreService = genreService;
-    }
 
     @GetMapping("/genre")
     protected List<Genre> findAll() {
--- src/main/java/com/bondarenko/movieland/web/controller/MovieController.java
+++ src/main/java/com/bondarenko/movieland/web/controller/MovieController.java
@@ -2,27 +2,23 @@ package com.bondarenko.movieland.web.controller;
 
 import com.bondarenko.movieland.entity.Movie;
 import com.bondarenko.movieland.service.MovieService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/movie", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MovieController {
 
     private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
-
     @GetMapping
-    protected List<Movie> getAll() {
-        return movieService.findAll();
+    protected List<Movie> getAll(@RequestParam Map<String, String> requestParameters) {
+        return movieService.findAll(requestParameters);
     }
 
     @GetMapping("/random")
@@ -31,7 +27,7 @@ public class MovieController {
     }
 
     @GetMapping("/genre/{genreId}")
-    protected List<Movie> getByGenre(@PathVariable("genreId") int genreId) {
-        return movieService.getByGenre(genreId);
+    protected List<Movie> getByGenre(@PathVariable("genreId") int genreId, @RequestParam Map<String, String> requestParameters) {
+        return movieService.getByGenre(genreId, requestParameters);
     }
 }
