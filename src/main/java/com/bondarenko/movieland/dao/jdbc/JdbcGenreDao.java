f1612d002d6544cb87b8775c3e1b369e4430d989
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-08 22:07:28 +0200

    add log.info
    - add log.info to JdbcGenreDao.

--- src/main/java/com/bondarenko/movieland/dao/jdbc/JdbcGenreDao.java
+++ src/main/java/com/bondarenko/movieland/dao/jdbc/JdbcGenreDao.java
@@ -4,11 +4,13 @@ import com.bondarenko.movieland.dao.GenreDao;
 import com.bondarenko.movieland.dao.jdbc.mapper.GenreRowMapper;
 import com.bondarenko.movieland.entity.Genre;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class JdbcGenreDao implements GenreDao {
@@ -19,6 +21,7 @@ public class JdbcGenreDao implements GenreDao {
 
     @Override
     public List<Genre> findAll() {
+        log.info("Find all genres");
         return jdbcTemplate.query(FIND_ALL_GENRES_SQL, genreRowMapper);
     }
 }
