1d0ec19425e0a2869d17f46f0699c9c345ac8ea5
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-08 19:52:35 +0200

    [iteration 1, b5] - implement sorting
    - add methods getByPrice, getByRating, getSortedMovies to DefaultMovieService;
    - refactor methods findAll, getByGenre inDefaultMovieService.

--- src/main/java/com/bondarenko/movieland/service/MovieService.java
+++ src/main/java/com/bondarenko/movieland/service/MovieService.java
@@ -3,12 +3,13 @@ package com.bondarenko.movieland.service;
 import com.bondarenko.movieland.entity.Movie;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MovieService {
 
-    List<Movie> findAll();
+    List<Movie> findAll(Map<String, String> requestParameters);
 
     List<Movie> getRandomMovies();
 
-    List<Movie> getByGenre(int genreId);
+    List<Movie> getByGenre(int genreId,Map<String, String> requestParameters);
 }
--- src/main/java/com/bondarenko/movieland/service/impl/DefaultMovieService.java
+++ src/main/java/com/bondarenko/movieland/service/impl/DefaultMovieService.java
@@ -1,38 +1,88 @@
 package com.bondarenko.movieland.service.impl;
 
 import com.bondarenko.movieland.dao.MovieDao;
-import com.bondarenko.movieland.entity.Genre;
 import com.bondarenko.movieland.entity.Movie;
-import com.bondarenko.movieland.exceptions.GenreNotFoundException;
+import com.bondarenko.movieland.exceptions.MovieNotFoundException;
 import com.bondarenko.movieland.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class DefaultMovieService implements MovieService {
 
-    private final int randomMoviesLength = 3;
+    private final int RANDOM_MOVIES_LENGTH = 3;
+    private final String RATING_PARAMETER = "rating";
+    private final String PRICE_PARAMETER = "price";
+    private final String ASC_PARAMETER = "asc";
+    private final String DESC_PARAMETER = "desc";
 
-    @Autowired
-    private MovieDao movieDao;
+    private final MovieDao movieDao;
 
     @Override
-    public List<Movie> findAll() {
-        return movieDao.findAll();
+    public List<Movie> findAll(Map<String, String> requestParameters) {
+        List<Movie> movies = movieDao.findAll();
+        if (!requestParameters.isEmpty()) {
+            return getSortedMovies(requestParameters, movies);
+        }
+        return movies;
     }
 
     @Override
     public List<Movie> getRandomMovies() {
         List<Movie> movies = movieDao.findAll();
         Collections.shuffle(movies);
-        return movies.subList(0, randomMoviesLength);
+        return movies.subList(0, RANDOM_MOVIES_LENGTH);
     }
 
     @Override
-    public List<Movie> getByGenre(int genreId) {
-        return movieDao.findByGenreId(genreId);
+    public List<Movie> getByGenre(int genreId, Map<String, String> requestParameters) {
+        List<Movie> moviesByGenre = movieDao.findByGenreId(genreId);
+        if (!requestParameters.isEmpty()) {
+            return getSortedMovies(requestParameters, moviesByGenre);
+        }
+        return moviesByGenre;
+    }
+
+    private List<Movie> getSortedMovies(Map<String, String> queryParameters, List<Movie> movies) {
+
+        String firstParameter = queryParameters.keySet().stream().findFirst().get();
+        String secondParameter = queryParameters.values().stream().findFirst().get();
+
+        if (firstParameter.equals(RATING_PARAMETER)) {
+            return getByRating(movies, secondParameter);
+        }
+
+        if (firstParameter.equals(PRICE_PARAMETER)) {
+            return getByPrice(movies, secondParameter);
+        }
+        throw new MovieNotFoundException(firstParameter);
+    }
+
+    private List<Movie> getByRating(List<Movie> movies, String requestParameter) {
+        if (requestParameter.equals(DESC_PARAMETER)) {
+            return movies.stream()
+                    .sorted(Comparator.comparing(Movie::getRating).reversed())
+                    .toList();
+        }
+        throw new MovieNotFoundException(requestParameter);
+    }
+
+    private List<Movie> getByPrice(List<Movie> movies, String requestParameter) {
+        if (requestParameter.equals(DESC_PARAMETER)) {
+            return movies.stream()
+                    .sorted(Comparator.comparing(Movie::getPrice).reversed())
+                    .toList();
+        }
+        if (requestParameter.equals(ASC_PARAMETER)) {
+            return movies.stream()
+                    .sorted(Comparator.comparing(Movie::getPrice).reversed().reversed()).toList();
+        }
+        throw new MovieNotFoundException(requestParameter);
     }
 }
