728545d0016b472716b05c110faf92c67e5ef0ce
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-18 20:41:51 +0200

    add test
    - add GenreControllerITest.

---
+++ src/test/java/com/bondarenko/movieland/web/controller/GenreControllerITest.java
@@ -0,0 +1,69 @@
+package com.bondarenko.movieland.web.controller;
+
+import com.bondarenko.movieland.AbstractWebITest;
+import com.bondarenko.movieland.cache.GenreCache;
+import com.bondarenko.movieland.entity.Genre;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class GenreControllerITest extends AbstractWebITest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    GenreCache cache;
+
+    @Test
+    @DisplayName("when Get All Genres with Correct Url then Ok Status Returned")
+    void whenGetGenres_withCorrectUrl_thenOkStatusReturned() throws Exception {
+        Genre genreFirst = Genre.builder()
+                .genreId(1)
+                .name("драма")
+                .build();
+
+        Genre genreSecond = Genre.builder()
+                .genreId(2)
+                .name("криминал")
+                .build();
+
+        when(cache.getCachedGenre()).thenReturn(List.of(genreFirst, genreSecond));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/genre")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("""
+                                [
+                                   {
+                                      "genreId":1,
+                                      "name":"драма"
+                                   },
+                                   {
+                                      "genreId":2,
+                                      "name":"криминал"
+                                   }
+                                ]"""))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("when Get Genres with Incorrect Url then Not Found Returned")
+    void whenGetGenres_withIncorrectUrl_thenNotFoundReturned() throws Exception {
+        when(cache.getCachedGenre()).thenReturn(List.of(new Genre(1, "криминал")));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/genres")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
