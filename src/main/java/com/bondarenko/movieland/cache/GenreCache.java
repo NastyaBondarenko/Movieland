7db8f8369c3a28ce05c72e5d787f0ef0ed771546
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-18 20:08:09 +0200

    refactor GenreCache
    - add annotation  @Transactional(readOnly = true) to enrichCache().

--- src/main/java/com/bondarenko/movieland/cache/GenreCache.java
+++ src/main/java/com/bondarenko/movieland/cache/GenreCache.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -15,18 +16,19 @@ import java.util.concurrent.TimeUnit;
 @Component
 @RequiredArgsConstructor
 public class GenreCache {
-    private volatile List<Genre> genresList;
+    private volatile List<Genre> genres;
     private final GenreRepository genreRepository;
 
     @PostConstruct
     @Scheduled(fixedRate = 4, initialDelay = 4, timeUnit = TimeUnit.HOURS)
+    @Transactional(readOnly = true)
     public void enrichCache() {
-        genresList = genreRepository.findAll();
-        log.info("Enrich genre cache, total genres in cache {} ", genresList.size());
+        genres = genreRepository.findAll();
+        log.info("Enrich genre cache, total genres in cache {} ", genres.size());
     }
 
     public List<Genre> getCachedGenre() {
-        log.info("Load {} genres from cache", genresList.size());
-        return genresList;
+        log.info("Load {} genres from cache", genres.size());
+        return genres;
     }
 }
