d20751c0d15acad9dfd85bc3c04236cfd123e973
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-19 19:44:09 +0200

    refactor GenreCache
    - add volatile list;
    - refactor migration.

--- src/main/java/com/bondarenko/movieland/cache/GenreCache.java
+++ src/main/java/com/bondarenko/movieland/cache/GenreCache.java
@@ -11,28 +11,25 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GenreCache implements GenreRepository {
-    private final CopyOnWriteArrayList<Genre> genresList = new CopyOnWriteArrayList<>();
+    private volatile List<Genre> genres;
     private final DefaultGenreRepository defaultGenreRepository;
 
     @Override
     public List<Genre> findAll() {
-        log.info("Get {} genres from cache", genresList.size());
-        return new ArrayList<>(genresList);
+        log.info("Get {} genres from cache", genres.size());
+        return new ArrayList<>(genres);
     }
 
     @PostConstruct
     @Scheduled(fixedRate = 4, initialDelay = 4, timeUnit = TimeUnit.HOURS)
     private void enrichCache() {
-        List<Genre> genres = defaultGenreRepository.findAll();
-        genresList.clear();
-        log.info("Enrich genre cache, genres in cache {} ", genresList.size());
-        genresList.addAll(genres);
+        genres = defaultGenreRepository.findAll();
+        log.info("Enrich genre cache, genres in cache {} ", genres.size());
     }
 }
