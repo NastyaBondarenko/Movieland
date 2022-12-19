f86202367783391f6682afe3f00bca8b5098bb31
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-19 20:43:30 +0200

    refactor GenreCache
    - refactor log.info in enrichCache();

--- src/main/java/com/bondarenko/movieland/cache/GenreCache.java
+++ src/main/java/com/bondarenko/movieland/cache/GenreCache.java
@@ -6,12 +6,14 @@ import com.bondarenko.movieland.repository.GenreRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -25,8 +27,8 @@ public class GenreCache implements GenreRepository {
     @Scheduled(fixedRate = 4, initialDelay = 4, timeUnit = TimeUnit.HOURS)
     @Transactional(readOnly = true)
     public void enrichCache() {
-        log.info("Enrich genre cache, genres in cache {} ", genres.size());
         genres = defaultGenreRepository.findAll();
+        log.info("Enrich genre cache, genres in cache {} ", genres.size());
     }
 
     @Override
