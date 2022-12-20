15e0ea6e9b5bfe93ca522dc95212c54bdad7cf12
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-20 22:50:51 +0200

    refactor exception
    - delete unused method MovieNotFoundException;

--- src/main/java/com/bondarenko/movieland/exceptions/MovieNotFoundException.java
+++ src/main/java/com/bondarenko/movieland/exceptions/MovieNotFoundException.java
@@ -8,8 +8,4 @@ public class MovieNotFoundException extends RuntimeException {
     public MovieNotFoundException(String parameter) {
         super(String.format(NO_MOVIE_SORTED_BY_PARAMETER_MESSAGE, parameter));
     }
-
-    public MovieNotFoundException(String parameter) {
-        super(String.format(NO_MOVIE_SORTED_BY_PARAMETER_MESSAGE, parameter));
-    }
 }
