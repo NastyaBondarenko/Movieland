9376616467903116fa5ac7475675362bd4d83449
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-20 19:34:21 +0200

    refactor pom.xml
    -delete unused dependencies;

--- src/main/java/com/bondarenko/movieland/cache/GenreCache.java
+++ src/main/java/com/bondarenko/movieland/cache/GenreCache.java
@@ -6,14 +6,12 @@ import com.bondarenko.movieland.repository.GenreRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
