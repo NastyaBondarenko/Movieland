83f2a79de7ecda6dd2bc38235351750cfe90ec6f
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-22 23:11:55 +0200

    refactor genreRepository
    - delete DefaultGenreRepository
    -  add GenreRepository to GenreCache.

--- src/main/java/com/bondarenko/movieland/repository/GenreRepository.java
+++ src/main/java/com/bondarenko/movieland/repository/GenreRepository.java
@@ -1,12 +1,13 @@
 package com.bondarenko.movieland.repository;
 
 import com.bondarenko.movieland.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 
-public interface GenreRepository {
+public interface GenreRepository extends JpaRepository<Genre, Integer> {
     List<Genre> findAll();
 
     Optional<Genre> findGenreById(int id);
