0b7b6c24bdaaeef5da4eca006597d9dc0616d5d3
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-12-26 18:53:28 +0200

    refactor migrations -review, users
    - add user_id_sequence to users table;
    - add constraints to review table;
    - delete migration V4.2

--- src/main/resources/db/migration/V1.0__Create_users_table.sql
+++ src/main/resources/db/migration/V1.0__Create_users_table.sql
@@ -4,3 +4,7 @@ CREATE TABLE users(
     "email" VARCHAR(45) NOT NULL,
     "password" VARCHAR(100) NOT NULL
 );
+
+CREATE SEQUENCE user_id_sequence OWNED BY users.id;
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('user_id_sequence');
+ALTER SEQUENCE user_id_sequence INCREMENT BY 50;
--- src/main/resources/db/migration/V4.0__Create_review_table.sql
+++ src/main/resources/db/migration/V4.0__Create_review_table.sql
@@ -7,18 +7,8 @@ CREATE SEQUENCE review_id_sequence OWNED BY review.id;
 ALTER TABLE review ALTER COLUMN id SET DEFAULT nextval('review_id_sequence');
 ALTER SEQUENCE review_id_sequence INCREMENT BY 50;
 
---CREATE TABLE review
---(
---    id       INTEGER      NOT NULL,
---    movie_id INTEGER      NOT NULL,
---    user_id  INTEGER      NOT NULL,
---    description     VARCHAR(500) NOT NULL,
---    CONSTRAINT pk_review PRIMARY KEY (id)
---);
---
---
---ALTER TABLE review
---    ADD CONSTRAINT FK_REVIEW_ON_MOVIE FOREIGN KEY (movie_id) REFERENCES movie (id);
---
---ALTER TABLE review
---    ADD CONSTRAINT FK_REVIEW_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE review ADD COLUMN "movie_id" INTEGER;
+ALTER TABLE review ADD CONSTRAINT movie_id FOREIGN KEY (movie_id) REFERENCES movie (id);
+
+ALTER TABLE review ADD COLUMN "user_id" INTEGER;
+ALTER TABLE review ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id);
