b516c23a72eca87d1f3615d04f7cb98db15206f0
NastyaBondarenko <av_bondarenko@rambler.ru>
2023-01-04 11:07:17 +0200

    [s-1] refactor SecurityController
    -  rename controller

--- src/main/java/com/bondarenko/movieland/web/controller/SecurityController.java
+++ src/main/java/com/bondarenko/movieland/web/controller/SecurityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1")
-public class LoginController {
+public class SecurityController {
     private final SecurityService securityService;
 
     @PostMapping("/login")
