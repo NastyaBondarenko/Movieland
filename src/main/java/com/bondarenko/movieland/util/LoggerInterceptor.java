3046c58594fba24b79ff95ad51647321b680642f
NastyaBondarenko <av_bondarenko@rambler.ru>
2023-01-10 19:08:18 +0200

    [lg-2] Add user and requestId logging
    - refactor LoggerInterceptor.

--- src/main/java/com/bondarenko/movieland/util/LoggerInterceptor.java
+++ src/main/java/com/bondarenko/movieland/util/LoggerInterceptor.java
@@ -2,28 +2,34 @@ package com.bondarenko.movieland.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.UUID;
 
+@Slf4j
 @Component
 public class LoggerInterceptor implements HandlerInterceptor {
 
-    Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+        return true;
+    }
 
     @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object object) {
-        MDC.put("requestId", UUID.randomUUID().toString());
-        return true;
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object object, ModelAndView model) {
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request,
                                 HttpServletResponse response, Object object, Exception arg3) {
-        log.info("Request is complete for user " + MDC.get("email"));
+        log.info("Request is completed for user " + MDC.get("email"));
+        MDC.clear();
     }
 }
