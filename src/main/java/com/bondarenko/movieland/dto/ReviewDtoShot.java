28b461e99a3631d23c7d1ebe0b37745f371690bc
NastyaBondarenko <av_bondarenko@rambler.ru>
2023-01-10 15:00:11 +0200

    add mappers tests
    - add CountryMapperTest;
    - add ReviewMapperTest.

--- src/main/java/com/bondarenko/movieland/dto/ReviewDtoShot.java
+++ src/main/java/com/bondarenko/movieland/dto/ReviewDtoShot.java
@@ -1,10 +1,12 @@
 package com.bondarenko.movieland.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ReviewDtoShot {
     private int movieId;
     private String description;
