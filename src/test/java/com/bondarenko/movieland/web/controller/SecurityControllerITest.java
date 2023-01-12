ac41bf39df271a9328fe6fa3962a471c36563524
NastyaBondarenko <av_bondarenko@rambler.ru>
2023-01-12 12:35:53 +0200

    add tests for security
    - add tests to SecurityControllerITest.

--- src/test/java/com/bondarenko/movieland/web/controller/SecurityControllerITest.java
+++ src/test/java/com/bondarenko/movieland/web/controller/SecurityControllerITest.java
@@ -10,9 +10,9 @@ import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
@@ -23,13 +23,173 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DBRider
-@AutoConfigureMockMvc(addFilters = false)
-public class ControllerITest extends AbstractWebITest {
+public class SecurityControllerITest extends AbstractWebITest {
 
     @MockBean
     private GenreRepository genreRepository;
 
     @Test
+    @WithMockUser(roles = "ADMIN")
+    @DataSet(value = "datasets/movie/dataset_movies.yml", cleanAfter = true,
+            cleanBefore = true, skipCleaningFor = "flyway_schema_history")
+    @ExpectedDataSet("datasets/movie/dataset_update_movie.yml")
+    @DisplayName("when Update Movie then Updated Movie and Ok Status Returned")
+    void whenUpdateMovie_thenUpdatedMovie_andOkStatusReturned() throws Exception {
+        MovieRequestDto movieRequestDto = MovieRequestDto.builder()
+                .nameRussian("Побег")
+                .nameNative("The Shawshank Redemption")
+                .picturePath("https://images-na")
+                .countryIds(List.of(1))
+                .genreIds(List.of(1))
+                .build();
+        mockMvc.perform(put("/api/v1/movie/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(movieRequestDto)))
+                .andExpect(content()
+                        .json("""
+                                {
+                                "id":2,
+                                "nameRussian":"Побег",
+                                "nameNative":"The Shawshank Redemption",
+                                "yearOfRelease":1999,
+                                "description":"Обвиненный в страшном преступлении",
+                                "rating":8.9,
+                                "price":134.67,
+                                "picturePath":"https://images-na",
+                                "votes":100
+                                }
+                                 """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DataSet(value = "datasets/movie/dataset_add_movie.yml", cleanAfter = true,
+            cleanBefore = true, skipCleaningFor = "flyway_schema_history")
+    @ExpectedDataSet("datasets/movie/dataset_expected_add_movie.yml")
+    @DisplayName("when Add Movie then Added Car and Ok Status Returned")
+    void whenAddMovie_thenAddedCar_andOkStatusReturned() throws Exception {
+        MovieRequestDto movieRequestDto = MovieRequestDto.builder()
+                .nameRussian("Побег")
+                .nameNative("The Shawshank Redemption")
+                .yearOfRelease(1994)
+                .price(123.45)
+                .description("Успешный банкир Энди Дюфрейн")
+                .picturePath("https://images-na")
+                .countryIds(List.of(2))
+                .genreIds(List.of(2))
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/movie")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(movieRequestDto)))
+                .andExpect(content()
+                        .json("""                        
+                                {
+                                "id":1,
+                                "nameRussian":"Побег",
+                                "nameNative":"The Shawshank Redemption",
+                                "yearOfRelease":1994,
+                                "description":"Успешный банкир Энди Дюфрейн",
+                                "rating":0.0,
+                                "price":123.45,
+                                "picturePath":"https://images-na",
+                                "votes":0
+                                }
+                                 """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    @DataSet("datasets/review/dataset_reviews.yml")
+    @ExpectedDataSet("datasets/review/dataset_add_review.yml")
+    @DisplayName("when Add Review then Review Added and Ok Status Returned")
+    void whenAddReview_thenReviewAdded_andOkStatusReturned() throws Exception {
+        ReviewRequestDto reviewDto = ReviewRequestDto.builder()
+                .movieId(1)
+                .description("Гениальное кино!")
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/review")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(reviewDto)))
+                .andExpect(content()
+                        .json("""
+                                {
+                                   "id":1,
+                                   "description":"Гениальное кино!"
+                                }
+                                """))
+                .andExpect(status().isOk());
+
+    }
+
+
+    @Test
+    @WithMockUser("hasAnyRole('USER','ADMIN')")
+    @DataSet("datasets/country/dataset_countries.yml")
+    @ExpectedDataSet("datasets/country/dataset_countries.yml")
+    @DisplayName("when Get All Countries with Correct Url then Ok Status Returned")
+    void whenGetAllCountries_withCorrectUrl_thenOkStatusReturned() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/country")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content()
+                        .json("""
+                                [
+                                   {
+                                      "id":1,
+                                      "name":"США"
+                                   },
+                                   {
+                                      "id":2,
+                                      "name":"Франция"
+                                   },
+                                   {
+                                      "id":3,
+                                      "name":"Великобритания"
+                                   },
+                                   {
+                                      "id":4,
+                                      "name":"Италия"
+                                   }
+                                ]"""))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser("hasAnyRole('USER','ADMIN')")
+    @DisplayName("when Get All Genres with Correct Url then Ok Status Returned")
+    void whenGetGenres_withCorrectUrl_thenOkStatusReturned() throws Exception {
+        Genre genreFirst = new Genre(1, "драма");
+        Genre genreSecond = new Genre(2, "криминал");
+        Genre genreThird = new Genre(3, "комедия");
+
+        when(genreRepository.findAll()).thenReturn(List.of(genreFirst, genreSecond, genreThird));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/genre")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content()
+                        .json("""
+                                [
+                                   {
+                                      "id":1,
+                                      "name":"драма"
+                                   },
+                                   {
+                                      "id":2,
+                                      "name":"криминал"
+                                   },
+                                   {
+                                      "id":3,
+                                      "name":"комедия"
+                                   }
+                                ]"""))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser("hasAnyRole('USER','ADMIN')")
     @DataSet("datasets/movie/dataset_movies.yml")
     @ExpectedDataSet("datasets/movie/dataset_movies.yml")
     @DisplayName("when Get All Movies with Correct Url then Ok Status Returned")
@@ -66,6 +226,7 @@ public class ControllerITest extends AbstractWebITest {
     }
 
     @Test
+    @WithMockUser("hasAnyRole('USER','ADMIN')")
     @DataSet("datasets/movie/dataset_movies.yml")
     @DisplayName("when Get Movie By Id then Ok Status Returned")
     void whenGetMovieById_thenOkStatusReturned() throws Exception {
@@ -114,190 +275,5 @@ public class ControllerITest extends AbstractWebITest {
                                 """))
                 .andExpect(status().isOk());
     }
-
-    @Test
-    @DataSet(value = "datasets/movie/dataset_movies.yml", cleanAfter = true,
-            cleanBefore = true, skipCleaningFor = "flyway_schema_history")
-    @ExpectedDataSet("datasets/movie/dataset_update_movie.yml")
-    @DisplayName("when Update Movie then Updated Movie and Ok Status Returned")
-    void whenUpdateMovie_thenUpdatedMovie_andOkStatusReturned() throws Exception {
-        MovieRequestDto movieRequestDto = MovieRequestDto.builder()
-                .nameRussian("Побег")
-                .nameNative("The Shawshank Redemption")
-                .picturePath("https://images-na")
-                .countryIds(List.of(1))
-                .genreIds(List.of(1))
-                .build();
-        mockMvc.perform(put("/api/v1/movie/2")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(movieRequestDto)))
-                .andExpect(content()
-                        .json("""
-                                {
-                                "id":2,
-                                "nameRussian":"Побег",
-                                "nameNative":"The Shawshank Redemption",
-                                "yearOfRelease":1999,
-                                "description":"Обвиненный в страшном преступлении",
-                                "rating":8.9,
-                                "price":134.67,
-                                "picturePath":"https://images-na",
-                                "votes":100
-                                }
-                                 """))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DataSet(value = "datasets/movie/dataset_add_movie.yml", cleanAfter = true,
-            cleanBefore = true, skipCleaningFor = "flyway_schema_history")
-    @ExpectedDataSet("datasets/movie/dataset_expected_add_movie.yml")
-    @DisplayName("when Add Movie then Added Car and Ok Status Returned")
-    void whenAddMovie_thenAddedCar_andOkStatusReturned() throws Exception {
-        MovieRequestDto movieRequestDto = MovieRequestDto.builder()
-                .nameRussian("Побег")
-                .nameNative("The Shawshank Redemption")
-                .yearOfRelease(1994)
-                .price(123.45)
-                .description("Успешный банкир Энди Дюфрейн")
-                .picturePath("https://images-na")
-                .countryIds(List.of(2))
-                .genreIds(List.of(2))
-                .build();
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/movie")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(movieRequestDto)))
-                .andExpect(content()
-                        .json("""                        
-                                {
-                                "id":1,
-                                "nameRussian":"Побег",
-                                "nameNative":"The Shawshank Redemption",
-                                "yearOfRelease":1994,
-                                "description":"Успешный банкир Энди Дюфрейн",
-                                "rating":0.0,
-                                "price":123.45,
-                                "picturePath":"https://images-na",
-                                "votes":0
-                                }
-                                 """))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DataSet("datasets/country/dataset_countries.yml")
-    @ExpectedDataSet("datasets/country/dataset_countries.yml")
-    @DisplayName("when Get All Countries with Correct Url then Ok Status Returned")
-    void whenGetAllCountries_withCorrectUrl_thenOkStatusReturned() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/country")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content()
-                        .json("""
-                                [
-                                   {
-                                      "id":1,
-                                      "name":"США"
-                                   },
-                                   {
-                                      "id":2,
-                                      "name":"Франция"
-                                   },
-                                   {
-                                      "id":3,
-                                      "name":"Великобритания"
-                                   },
-                                   {
-                                      "id":4,
-                                      "name":"Италия"
-                                   }
-                                ]"""))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("when Get All Genres with Correct Url then Ok Status Returned")
-    void whenGetGenres_withCorrectUrl_thenOkStatusReturned() throws Exception {
-        Genre genreFirst = new Genre(1, "драма");
-        Genre genreSecond = new Genre(2, "криминал");
-        Genre genreThird = new Genre(3, "комедия");
-
-        when(genreRepository.findAll()).thenReturn(List.of(genreFirst, genreSecond, genreThird));
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/genre")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content()
-                        .json("""
-                                [
-                                   {
-                                      "id":1,
-                                      "name":"драма"
-                                   },
-                                   {
-                                      "id":2,
-                                      "name":"криминал"
-                                   },
-                                   {
-                                      "id":3,
-                                      "name":"комедия"
-                                   }
-                                ]"""))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DataSet("datasets/review/dataset_reviews.yml")
-    @ExpectedDataSet("datasets/review/dataset_add_review.yml")
-    @DisplayName("when Add Review then Review Added and Ok Status Returned")
-    void whenAddReview_thenReviewAdded_andOkStatusReturned() throws Exception {
-        ReviewRequestDto reviewDto = ReviewRequestDto.builder()
-                .movieId(1)
-                .description("Гениальное кино!")
-                .build();
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/review")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reviewDto)))
-                .andExpect(content()
-                        .json("""
-                                {
-                                   "id":1,
-                                   "description":"Гениальное кино!"
-                                }
-                                """))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("when Get Genres with Incorrect Url then Not Found Returned")
-    void whenGetGenres_withIncorrectUrl_thenNotFoundReturned() throws Exception {
-        when(genreRepository.findAll()).thenReturn(List.of(new Genre(1, "криминал")));
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/genres")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("when Get All Movies with Incorrect Url then Not Found Returned")
-    void whenGetAllMovies_withIncorrectUrl_thenNotFoundReturned() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/movies")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("when Get Movie By Genre with Incorrect Url then Bad Request Return")
-    void whenGetMovieByGenre_withIncorrectUrl_thenBadRequestReturn() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/movie/genre/dd")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("when Get Random Movies with Incorrect Url then Not Found Return")
-    void whenGetRandomMovies_withIncorrectUrl_thenNotFoundReturn() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/movie/randoms")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
 }
+
