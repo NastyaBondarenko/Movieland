e6f4f3f922696503f4dbe79f7b270862e564b121
NastyaBondarenko <av_bondarenko@rambler.ru>
2023-02-02 15:27:39 +0200

    refactor Movie
    -  add @ManyToMany to genres, countries.

--- src/main/java/com/bondarenko/movieland/entity/Movie.java
+++ src/main/java/com/bondarenko/movieland/entity/Movie.java
@@ -1,24 +1,10 @@
 package com.bondarenko.movieland.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.OptimisticLockType;
 import org.hibernate.annotations.OptimisticLocking;
-import org.hibernate.annotations.SelectBeforeUpdate;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -64,12 +50,12 @@ public class Movie {
     @Column(name = "votes")
     private int votes;
 
-    @OneToMany
+    @ManyToMany
     @JoinTable(name = "movie_genre", joinColumns = @JoinColumn(name = "movie_id"),
             inverseJoinColumns = @JoinColumn(name = "genre_id"))
     private Set<Genre> genres = new HashSet<>();
 
-    @OneToMany
+    @ManyToMany
     @JoinTable(name = "movie_country", joinColumns = @JoinColumn(name = "movie_id"),
             inverseJoinColumns = @JoinColumn(name = "country_id"))
     private Set<Country> countries = new HashSet<>();
